6847e433627258c481e4e2c28fa7cf5f25c92950
tiagojulio <tiagojulio96@hotmail.com>
2022-06-18 14:08:10 -0300

    feat: finished application

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [{
+        id: 1,
+        title: 'Salário dev',
+        type: 'deposit',
+        category: 'Salário',
+        amount: 6000,
+        createdAt: new Date('2022-06-04 10:00:00'),
+      }, {
+        id: 2,
+        title: 'Contas',
+        type: 'withdraw',
+        category: 'Despesas',
+        amount: 600,
+        createdAt: new Date('2022-06-05 1:00:00'),
+      }]
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    })
+  }
+})
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
